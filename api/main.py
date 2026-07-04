b7bff0120c1a96bf6c01a275486b465fe8c9c70b
Pragathi Gayatri <ppragathigayatri@gmail.com>
2026-07-04 19:14:09 +0530

    week 3 Day 6-File upload API

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import os
 
 app = FastAPI()
 
@@ -6,3 +7,23 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "ContractIQ API Running Successfully"}
+
+
+@app.post("/upload")
+async def upload_contract(file: UploadFile = File(...)):
+
+    upload_folder = "uploads"
+
+    if not os.path.exists(upload_folder):
+        os.makedirs(upload_folder)
+
+    file_path = os.path.join(upload_folder, file.filename)
+
+    with open(file_path, "wb") as buffer:
+        content = await file.read()
+        buffer.write(content)
+
+    return {
+        "filename": file.filename,
+        "status": "File uploaded successfully"
+    }
