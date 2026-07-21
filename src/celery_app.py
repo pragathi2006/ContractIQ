1667521c025201bb6eff6e58226ea6f5161f73ae
Pragathi Gayatri <ppragathigayatri@gmail.com>
2026-07-21 12:47:06 +0530

    Load .env in celery_app so CELERY_BROKER_URL is picked up outside Docker

--- src/celery_app.py
+++ src/celery_app.py
@@ -1,6 +1,9 @@
 import os
 
 from celery import Celery
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0")
 RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
